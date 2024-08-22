4f3c46cafe2fe49a5652147dc0f94841f6dbb47c
haewook-lee <20hwlee@gmail.com>
2024-08-22 19:27:03 -0400

    Starter Tamagui/NextJS

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// @ts-check
+import { withTamagui }  from '@tamagui/next-plugin'
 
-export default nextConfig;
+export default (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    
+  }
+
+  const tamaguiPlugin = withTamagui({
+    config: './tamagui.config.ts',
+    components: ['tamagui'],
+    outputCSS: process.env.NODE_ENV === 'production' ? './public/tamagui.css' : null,
+    disableExtraction: process.env.NODE_ENV === 'development'
+  })
+
+  return {
+    ...nextConfig,
+    ...tamaguiPlugin(nextConfig)
+  }
+}
---
+++ tamagui.config.ts
@@ -0,0 +1,31 @@
+import { config } from '@tamagui/config/v3'
+
+import { createTamagui } from 'tamagui' // or '@tamagui/core'
+const appConfig = createTamagui(config)
+export type AppConfig = typeof appConfig
+declare module 'tamagui' {
+  // or '@tamagui/core'
+  // overrides TamaguiCustomConfig so your custom types
+  // work everywhere you import `tamagui`
+
+  interface TamaguiCustomConfig extends AppConfig {
+    // put tamagui configs here
+    media:{
+      sm: {maxWidth: 360},
+      md: {maxWidth: 768},
+      lg: {maxWidth: 1024},
+      xl: {maxWidth: 1440},
+      xxl: {maxWidth: 2560}
+    }
+  }
+  
+}
+export default appConfig
+
+// export const media = createTamagui({
+//   media:{
+//     sm: {maxWidth: 480},
+//     md: {maxWidth: 768},
+//     lg: {maxWidth: 1024}
+//   }
+// })
